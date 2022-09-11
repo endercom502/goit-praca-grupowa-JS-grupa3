fe488f4a4aa8446550ec4118c1ec526cee133819
MateuszJaz <mateusz.jaz@outlook.com>
2022-09-12 00:25:08 +0200

    dodano obsługę zapytań do api i renderowanie wyników wyszukiwania

--- src/js/search_api.js
+++ src/js/search_api.js
@@ -2,11 +2,14 @@ import axios from 'axios';
 
 const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/events';
 const API_KEY = 'zgJDbIZVlwZnbWttdYxA1sycG5ZV7RfO';
-let keyword = 'rage';
-let page = 0;
-let countryCode = 'PL';
 
-export const getEvents = (keyword, countryCode, page) => {
+const eventCard = document.querySelector('.event #event_post');
+
+let page = 0;
+let countryCode = 'pl';
+let keyword = '';
+
+const getEvents = (keyword, countryCode, page) => {
   const params = {
     apikey: API_KEY,
     countryCode: countryCode,
@@ -18,19 +21,34 @@ export const getEvents = (keyword, countryCode, page) => {
   return response;
 };
 
-const eventCard = document.querySelector('#event_post');
+getEvents(keyword, countryCode, page)
+  .then(function (response) {
+    if (response.data.page.totalElements === 0) {
+      console.log('No events found. Try different quote'); // dodać obsługę wyświetlenia komunikatu gdy brak rezultatów
+    } else {
+      //  console.log(response.data._embedded.events);
+      renderResults(response);
+    }
+  })
+  .catch(error => console.log(error));
 
-getEvents(keyword, countryCode, page).then(function (response) {
-  if (response.data.page.totalElements === 0) {
-    console.log('No events found. Try different quote');
-  } else {
-    console.log(response.data._embedded.events);
-    renderResults(response);
-  }
-});
+//renderowanie wyników wyszukiwania //
 
 function renderResults(response) {
-  const markup = response.data._embedded.events.map(({ name, dates }) => {
-    console.log(name, dates);
-  });
+  const markup = response.data._embedded.events
+    .map(({ images, name, dates, _embedded }) => {
+      return `
+        <li class="event_item">
+            <a class="event_item-link href="#">
+               <img class="event_item-image" src="${images?.[7].url}" alt="${name}" width="180" height="227" loading="lazy"/>
+                  <p class="event_item-name"><b>${name}</b></p>
+                  <p class="event_item-date"><b>${dates?.start.localDate}</b></p>
+                  <p class="event_item-venue"><b>${_embedded?.venues[0].name}</b></p>
+                  <p class="event_item-city"><b>${_embedded?.venues[0].city.name}</b></p>
+            </a>
+         </li>`;
+    })
+    .join('');
+
+  eventCard.insertAdjacentHTML('beforeend', markup);
 }
