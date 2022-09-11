7c844206cdd271b5b0d4903599a573d2c699eabe
MateuszJaz <mateusz.jaz@outlook.com>
2022-09-11 18:41:49 +0200

    dodano render results

--- src/js/search_api.js
+++ src/js/search_api.js
@@ -2,26 +2,35 @@ import axios from 'axios';
 
 const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/events';
 const API_KEY = 'zgJDbIZVlwZnbWttdYxA1sycG5ZV7RfO';
-let keyword = 'London';
-let page = 1;
-let countryCode = '';
+let keyword = 'rage';
+let page = 0;
+let countryCode = 'PL';
 
-const searchByName = (keyword, countryCode, page) => {
+export const getEvents = (keyword, countryCode, page) => {
   const params = {
     apikey: API_KEY,
+    countryCode: countryCode,
     keyword: keyword,
     page: page,
   };
-  if (countryCode.length) {
-    params.countryCode = countryCode;
-  }
 
-  return axios.get(`${BASE_URL}`, { params });
+  const response = axios.get(`${BASE_URL}`, { params });
+  return response;
 };
 
 const eventCard = document.querySelector('#event_post');
 
-searchByName(keyword, countryCode, page).then(function (response) {
-  eventCard.innerHTML = response.data._embedded.events;
-  console.log(response.data._embedded.events);
+getEvents(keyword, countryCode, page).then(function (response) {
+  if (response.data.page.totalElements === 0) {
+    console.log('No events found. Try different quote');
+  } else {
+    console.log(response.data._embedded.events);
+    renderResults(response);
+  }
 });
+
+function renderResults(response) {
+  const markup = response.data._embedded.events.map(({ name, dates }) => {
+    console.log(name, dates);
+  });
+}
