78662ad2212ba4eeac9ead84339ae82b7f995c35
MateuszJaz <mateusz.jaz@outlook.com>
2022-09-11 16:42:58 +0200

    Update search_api.js

--- src/js/search_api.js
+++ src/js/search_api.js
@@ -1,23 +1,27 @@
-import axios from "axios";
+import axios from 'axios';
 
-const BASE_URL = "https://app.ticketmaster.com/discovery/v2/events";
+const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/events';
 const API_KEY = 'zgJDbIZVlwZnbWttdYxA1sycG5ZV7RfO';
+let keyword = 'London';
+let page = 1;
+let countryCode = '';
 
-const searchByName = (keyword, countryCode,page) =>{
-   const params = {
-      apikey: API_KEY,
-      keyword:keyword,
-      countryCode: countryCode,
-      page:page,
-    };
+const searchByName = (keyword, countryCode, page) => {
+  const params = {
+    apikey: API_KEY,
+    keyword: keyword,
+    page: page,
+  };
+  if (countryCode.length) {
+    params.countryCode = countryCode;
+  }
 
-   return axios.get(`${BASE_URL}`,{params});
+  return axios.get(`${BASE_URL}`, { params });
 };
 
-const eventCard = document.querySelector('#event_post')
+const eventCard = document.querySelector('#event_post');
 
-searchByName ('london','UK')
-.then(function (response) {
-   eventCard.innerHTML = response.data._embedded.events
-   console.log(response.data._embedded.events)
-})
+searchByName(keyword, countryCode, page).then(function (response) {
+  eventCard.innerHTML = response.data._embedded.events;
+  console.log(response.data._embedded.events);
+});
