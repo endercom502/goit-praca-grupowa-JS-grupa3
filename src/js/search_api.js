298fb49caeb85e3d7aec12ef985f77df1252dd0f
wombatstar <toxaf13@gmail.com>
2022-09-11 14:03:46 +0200

    1.1.2

--- src/js/search_api.js
+++ src/js/search_api.js
@@ -3,11 +3,12 @@ import axios from "axios";
 const BASE_URL = "https://app.ticketmaster.com/discovery/v2/events";
 const API_KEY = 'zgJDbIZVlwZnbWttdYxA1sycG5ZV7RfO';
 
-const searchByName = (keyword, countryCode) =>{
+const searchByName = (keyword, countryCode,page) =>{
    const params = {
       apikey: API_KEY,
       keyword:keyword,
       countryCode: countryCode,
+      page:page,
     };
 
    return axios.get(`${BASE_URL}`,{params});
@@ -15,21 +16,8 @@ const searchByName = (keyword, countryCode) =>{
 
 const eventCard = document.querySelector('#event_post')
 
-searchByName ('')
+searchByName ('london','UK')
 .then(function (response) {
-   eventCard.innerHTML = response.data.map(function (event) {
-     return (
-       '<li class="row">' +
-         '<img src="https://avatars.githubusercontent.com/u/' + event.data._embedded.events.name + '?s=50" class="col-md-1"/>' +
-         '<div class="col-md-3">' +
-           '<strong>' + person.name + '</strong>' +
-           '<div>Github: <a href="https://github.com/' + person.github + '" target="_blank">' + person.github + '</a></div>' +
-           '<div>Twitter: <a href="https://twitter.com/' + person.twitter + '" target="_blank">' + person.twitter + '</a></div>' +
-         '</div>' +
-       '</li><br/>'
-     );
-   }).join('');
- })
- .catch(function (err) {
-   eventCard.innerHTML = '<li class="text-danger">' + err.message + '</li>';
- });
+   eventCard.innerHTML = response.data._embedded.events
+   console.log(response.data._embedded.events)
+})
