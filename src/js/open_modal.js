2fef2946d254f1c2a7961de72ec017f4bc0db0b9
Andrzej Markiewicz <endercom502@gmail.com>
2022-09-15 21:48:33 +0200

    re

--- src/js/open_modal.js
+++ src/js/open_modal.js
@@ -1,7 +1,16 @@
-// import $ from 'jquery';
-const modalBody = document.querySelector('[data-modal]');
-// const openModal = document.getElementsByClassName('event_item');
-// openModal.addEventListener('click', () => {
-modalBody.removeClass('is-hiddenn');
-// });
-console.log(modalBody);
+const openModal = document.querySelector('.event #event_post');
+const modalBody = document.querySelector('.modal__backdrop');
+const modalClose = document.querySelector('.modal__esc');
+const hiden = hiden => {
+  modalBody.classList.remove('is-hiddenn');
+};
+const close = close => {
+  modalBody.classList.add('is-hiddenn');
+};
+
+openModal.addEventListener('click', hiden);
+modalClose.addEventListener('click', close);
+modalClose.addEventListener('keydown', event => {
+  console.log('key: ', event.key);
+  console.log('code: ', event.code);
+});
