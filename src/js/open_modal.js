eb778c88135cd0f903d9e8c8a75cfc2f30781002
Andrzej Markiewicz <endercom502@gmail.com>
2022-09-17 13:25:45 +0200

    fg

--- src/js/open_modal.js
+++ src/js/open_modal.js
@@ -14,6 +14,37 @@ export const getEvents1 = (keyword1, countryCode1, page1) => {
   const response1 = axios.get(`${BASE_URL}`, { params1 });
   return response1;
 };
+getEvents(keyword, countryCode, page)
+  .then(function (response) {
+    if (response.data.page.totalElements === 0) {
+      console.log('No events found. Try different quote'); // dodać obsługę wyświetlenia komunikatu gdy brak rezultatów
+    } else {
+      console.log(response.data._embedded.events);
+      renderResults(response);
+    }
+  })
+  .catch(error => console.log(error));
+
+//renderowanie wyników wyszukiwania //
+
+// // function renderResults(response1) {
+// //   const markup = response1.data._embedded.events
+// //     .map(({ images, name, dates, _embedded }) => {
+// //       return `
+// //         <li class="event_item">
+// //             <a class="event_item-link href="#">
+// //                <img class="event_item-image" src="${images?.[7].url}" alt="${name}" width="180" height="227" loading="lazy"/>
+// //                   <p class="event_item-name"><b>${name}</b></p>
+// //                   <p class="event_item-date"><b>${dates?.start.localDate}</b></p>
+// //                   <p class="event_item-venue"><b>${_embedded?.venues[0].name}</b></p>
+// //                   <p class="event_item-city"><b>${_embedded?.venues[0].city.name}</b></p>
+// //             </a>
+// //          </li>`;
+// //     })
+// //     .join('');
+
+// //   eventNameToModal.insertAdjacentHTML('beforeend', markup);
+// }
 
 // open modal
 const openModal = document.querySelector('.event #event_post');
