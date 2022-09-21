7f0b6ce15aaea8ae3fd87755e0bf178caa9c042f
MateuszJaz <mateusz.jaz@outlook.com>
2022-09-21 04:57:28 +0200

    poprawki

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -23,18 +23,6 @@ export let options = {
     page: '<a href="#" class="tui-page-btn">{{page}}</a>',
     currentPage:
       '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-    moveButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}">' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      '</a>',
-    disabledMoveButton:
-      '<span class="hidden tui-page-btn tui-is-disabled tui-{{type}}" hidden>' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      '</span>',
-    moreButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-      '<span class="tui-ico-ellip">...</span>' +
-      '</a>',
   },
 };
 
